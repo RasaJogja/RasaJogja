62961ff98c6fe382b7db0ec623204962c0cea16b
aminardisy <ardi.dadih27@gmail.com>
2024-10-24 09:59:15 +0700

    tes branch

--- autentikasi/models.py
+++ autentikasi/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Autentikasi(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_type = models.CharField(max_length=255)
 # Create your models here.

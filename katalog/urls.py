f26c60bec2319437005d1f4afec8f869c2d82c9a
clarissaindriana <clarissaindrianapramesti@gmail.com>
2024-10-25 20:18:18 +0700

    newest card for catalog

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,12 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from katalog.views import product_list, main_view
+from katalog.views import main_view
 
 app_name= 'katalog'
 
 urlpatterns = [
-    path('product-list/', product_list, name='product_list'),
     path('main-view/', main_view, name='main_view'),
   
 ]

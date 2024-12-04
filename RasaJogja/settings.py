ed75ad1cca21b6cf927f80d1defea46b73dbe959
aminardisy <ardi.dadih27@gmail.com>
2024-12-04 14:17:04 +0700

    autentikasi flutter

--- RasaJogja/settings.py
+++ RasaJogja/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dj+gg6i-r#af6fux^=tb91fl9p$tscmnlw=-g7atl+4foms6b#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "clarissa-indriana-rasajogja.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "clarissa-indriana-rasajogja.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'forum',
     'review',
     'bookmark',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'RasaJogja.urls'
@@ -134,3 +136,10 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.views.generic import RedirectView  # Import RedirectView
 from main.views import login_user, register, logout_user
 from main.views import show_auth, show_mainpage
+from main.views import login, register_flutter
 
 app_name = 'main'
 
@@ -11,4 +12,6 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('mainpage/', show_mainpage, name='show_mainpage'),
+    path('login_flutter/', login, name='login_flutter'),
+    path('register_flutter/', register_flutter, name='register_flutter')
 ]

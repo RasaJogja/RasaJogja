5c7bf6db3678fea21059cf4e4495964179df4fd9
inayadiza <mainah.diza@gmail.com>
2024-12-19 22:34:03 +0700

    ini buat flutter

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from forum.views import create_forum_entry, show_main, add_comment, delete_forum
+from forum.views import add_comment_flutter, create_forum_entry, show_main, add_comment, delete_forum, show_json_forum, delete_forum_flutter, create_forum_flutter, show_json_comment_forum
 
 app_name = 'forum'
 
@@ -8,4 +8,9 @@ urlpatterns = [
     path('create-forum-entry/', create_forum_entry, name='create_forum_entry'),
     path('add-comment/<uuid:forum_entry_id>/', add_comment, name='add_comment'),
     path('delete/<uuid:id>', delete_forum, name='delete_forum'),
+    path('get-forum-entries/', show_json_forum, name='get_forum_entries'),
+    path('delete-forum-flutter/<uuid:forum_id>/', delete_forum_flutter, name='delete_forum_flutter'),
+    path('create-forum-flutter/', create_forum_flutter, name='create_forum_flutter'),
+    path('add_comment_flutter/<uuid:forum_id>/', add_comment_flutter, name='add_comment_flutter'),
+    path('show-json-comment/<uuid:forum_id>/', show_json_comment_forum, name='show_json_comment')
 ]

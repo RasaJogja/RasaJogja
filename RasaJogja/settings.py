de05755a2b042fd5e687769825f433c46e559e80
Naurah15 <naunaurahiradya@gmail.com>
2024-12-21 18:09:43 +0700

    review selesai

--- RasaJogja/settings.py
+++ RasaJogja/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'chat',
     'forum',
     'review',
-    'bookmark',
     'authentication',
     'corsheaders',
 ]
--- review/urls.py
+++ review/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from review.views import show_review, add_review
+from review.views import show_review, add_review, get_product_reviews_json, add_review_json
 
 app_name = 'review'
 urlpatterns = [
     path('product/<int:pk>/', show_review, name='show_review'),
     path('product/<int:pk>/add/', add_review, name='add_review'),
+
+    path('api/product/<int:pk>/reviews/', get_product_reviews_json, name='get_product_reviews_json'),
+    path('api/product/<int:pk>/add/', add_review_json, name='add_review_json'),
 ]
